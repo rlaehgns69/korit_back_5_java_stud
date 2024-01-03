23a3e45f76cf60601012bbd74f08bc39750d7fa4
rlaehgns69 <rlaehgns69@naver.com>
2024-01-03 17:48:18 +0900

    5:50 추가

---
+++ src/main/java/com/study/ch06/For02.java
@@ -0,0 +1,57 @@
+package com.study.ch06;
+
+public class For02 {
+    public static void main(String[] args) {
+        /*
+        0, 1, 2, 3, 4
+         */
+        for (int i = 0; i < 5; i++) {
+            System.out.print(i);
+            if(i % 5 != 4){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+//        for(int i = 0; i < 5; i++){
+//            if(i == 4){
+//                System.out.println(i);
+//                break;
+//            }
+//            System.out.print(i+", ");
+//        }
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i);
+            if(i % 5 !=4) {
+                System.out.print(", ");
+            }
+            if(i % 5 == 4) {
+                System.out.println();
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            System.out.print(4-i);
+            if(i % 5 != 4){
+                System.out.print(", ");
+            }
+            if(i % 5 == 4) {
+                System.out.println();
+            }
+        }
+        int n = 10;
+
+        for (int i = 0; i < n; i++) {
+            int rv_i = n- 1 -i;
+            System.out.print(rv_i);
+            if(rv_i % 5 != 0) {
+                System.out.print(", ");
+            }
+            if(rv_i % 5 == 0) {
+                System.out.println();
+            }
+
+
+
+        }
+    }
+}
---
+++ src/main/java/com/study/ch06/For03.java
@@ -0,0 +1,36 @@
+package com.study.ch06;
+
+import java.util.Scanner;
+
+public class For03 {
+    /*
+
+    *
+    **
+    *** 이거함
+
+      *
+     **
+    ***
+
+    ***
+     **
+      *
+
+      *
+     ***
+    *****
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("별 몇개?");
+        int starCount = scanner.nextInt();
+
+        for (int i = 0; i < starCount; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
